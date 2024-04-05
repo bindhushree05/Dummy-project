54cd71b2cb7839747d2645c9628088784b827c2b
bindhushree <bindhushreeb05@gmail.com>
2024-04-05 13:18:07 +0530

    changes done in server.js with respect to mongoDB

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PUBLIC_PORT || 3000;
+const mongodburi = process.env.MONGODB_URI
 
+//Get request
 app.get('/', (req, res) => {
   res.json({ message: 'pong' }); 
 });
+
+
+//connecting mongodb
+mongoose.connect(mongodburi)
+
+// Defining a route to check the mongoDB connection satus
+app.get('/mongoDBConnection', (req, res) => {
+  if(mongoose.connection.readyState === 1){
+    res.status(200).json('MongoDB connection is successfull')
+  }else{
+    res.status(400).json('error connection in MongoDB')
+  }
+});
+
 
 if (require.main === module) {
   app.listen(port, () => {
